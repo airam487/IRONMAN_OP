13582afd5d02ca97b18d4d795bb68527da8c9d3e
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-16 16:21:24 +0530

    Add files via upload

---
+++ Musical/modules/userbotjoin.py
@@ -0,0 +1,74 @@
+# Musical (Telegram bot project )
+# Copyright (C) 2021  Inukaasith
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.errors import UserAlreadyParticipant
+import asyncio
+from Musical.helpers.decorators import authorized_users_only
+from Musical.helpers.decorators import errors
+from Musical.services.callsmusic import client as USER
+from Musical.config import SUDO_USERS
+
+@Client.on_message(filters.command(["join"]) & ~filters.private & ~filters.bot)
+@authorized_users_only
+@errors
+async def addchannel(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "<b>Add me as admin of yor group first</b>",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "DaisyMusic"
+
+    try:
+        await USER.join_chat(invitelink)
+        await USER.send_message(message.chat.id, "I joined here as you requested")
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>helper already in your chat</b>",
+        )
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
+            "\n\nOr manually add your assistance to your Group and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>helper userbot joined your chat</b>",
+    )
+
+
+@USER.on_message(filters.group & filters.command(["ubleave"]))
+@authorized_users_only
+async def rem(USER, message):
+    try:
+        await USER.leave_chat(message.chat.id)
+    except:
+        await message.reply_text(
+            f"<b>User couldn't leave your group! May be floodwaits."
+            "\n\nOr manually kick me from to your Group</b>",
+        )
+        return
