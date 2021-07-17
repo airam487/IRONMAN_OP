737c075648eb5bb30566da8f99f57152a4161415
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-17 10:13:13 +0530

    Update __main__.py

--- Musical/__main__.py
+++ Musical/__main__.py
@@ -1,11 +1,11 @@
 import requests
 from pyrogram import Client as Bot
 
-from Music.config import API_HASH
-from Music.config import API_ID
+from Musical.config import API_HASH
+from Musical.config import API_ID
 from Music.config import BG_IMAGE
-from Music.config import BOT_TOKEN
-from Music.services.callsmusic import run
+from Musical.config import BOT_TOKEN
+from Musical.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -17,7 +17,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="Music.modules"),
+    plugins=dict(root="Musical.modules"),
 )
 
 bot.start()
