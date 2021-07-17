a98fd903659911f87889bdc8110eced8922b3121
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-17 10:18:51 +0530

    Update __init__.py

--- Musical/function/__init__.py
+++ Musical/function/__init__.py
@@ -1,5 +1,5 @@
-from Music.function.admins import admins
-from Music.function.admins import get
-from Music.function.admins import set
+from Musical.function.admins import admins
+from Musical.function.admins import get
+from Musical.function.admins import set
 
 __all__ = ["set", "get", "admins"]
